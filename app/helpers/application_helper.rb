163a4b6f21ae95cd75f77921b7499e80d28a7bd4
cliftondavies <clifton.davies@outlook.com>
2020-07-30 23:22:06 +0100

    Set up helper for nav links on application layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def top_links
+    if signed_in?
+      (link_to "Welcome,  #{current_user.name}", root_path) +
+        (link_to 'Sign Out', session_path, method: :delete)
+    else
+      (link_to 'Sign In', new_session_path) +
+        (link_to 'Sign Up', new_user_path)
+    end
+  end
 end
