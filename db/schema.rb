e66effbef50d02e881e755e31ac54e43a3669467
cliftondavies <clifton.davies@outlook.com>
2020-07-31 23:54:49 +0100

    Migrate Rsvp model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_31_191257) do
+ActiveRecord::Schema.define(version: 2020_07_31_211928) do
 
   create_table "events", force: :cascade do |t|
     t.text "description"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2020_07_31_191257) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "rsvps", force: :cascade do |t|
+    t.integer "attendee_id"
+    t.integer "attended_event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["attended_event_id"], name: "index_rsvps_on_attended_event_id"
+    t.index ["attendee_id"], name: "index_rsvps_on_attendee_id"
   end
 
   create_table "users", force: :cascade do |t|
