5a2ee91bb7a4c17bf2ab098bea87600d4e301632
cliftondavies <clifton.davies@outlook.com>
2020-08-05 22:38:14 +0100

    Refactor Event #past and #upcoming into scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,11 +3,6 @@ class Event < ApplicationRecord
   has_many :attendees, through: :rsvps
   belongs_to :creator, class_name: 'User'
 
-  def self.upcoming
-    where('event_date >= ?', Date.today).order(:event_date)
-  end
-
-  def self.past
-    where('event_date < ?', Date.today).order(:event_date)
-  end
+  scope :upcoming, -> { where('event_date >= ?', Date.today).order(:event_date) }
+  scope :past, -> { where('event_date < ?', Date.today).order(:event_date) }
 end
