09a50f8ee65ae90b61416bcdb7a9c4697767d460
cliftondavies <clifton.davies@outlook.com>
2020-08-12 00:20:37 +0100

    Add integration tests for authentication and event creation

--- spec/system/event_managements_spec.rb
+++ spec/system/event_managements_spec.rb
@@ -1,9 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe "EventManagements", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
+RSpec.describe 'EventManagement', type: :system do
+  before { driven_by(:rack_test) }
+  let(:user) { create(:user) }
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe 'Create Event' do
+    context 'when signed in' do
+      before do
+        visit new_session_path
+        fill_in 'Username', with: user.username
+        click_button 'Log in'
+      end
+
+      scenario 'creates a new event' do
+        visit new_event_path
+        fill_in 'Description', with: 'Test Event'
+        click_button 'Create Event'
+        expect(page).to have_content 'Your Event was successfully created!'
+      end
+    end
+
+    context 'when signed out' do
+      scenario 'cannot create a new event' do
+        visit new_event_path
+        expect(page).to have_content 'You must be signed in to view this page.'
+      end
+    end
+  end
 end
