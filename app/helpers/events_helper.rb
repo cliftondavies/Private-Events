fd47d11c68804f7c40c70e3e0598f97a6872c8f2
cliftondavies <clifton.davies@outlook.com>
2020-08-15 19:38:43 +0100

    Implement view for Rsvp-ing to an event

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,9 @@
 module EventsHelper
+  def rsvp_status(event)
+    if signed_in? && current_user.rsvps.find_by(attended_event: event)
+      '<span>Attendance Confirmed</span>'.html_safe
+    else
+      render 'rsvps/form'
+    end
+  end
 end
