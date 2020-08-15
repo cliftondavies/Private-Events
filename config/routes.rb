27d54e46abe3ea2911c270fdec8f5e4ce47d4cb9
cliftondavies <clifton.davies@outlook.com>
2020-08-15 17:13:41 +0100

    Specify route for rsvps resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'rsvps/create'
-  resources :events, only: %i[index new create show]
+  # get 'rsvps/create'
+  resources :events, only: %i[index new create show] do
+    resources :rsvps, only: :create
+  end
   resources :users, only: %i[new create show]
   resources :sessions, only: %i[new create destroy]
   root 'events#index'
